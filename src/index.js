9b664e6331f66a480183976f71edad43fe149201
Piyush Kumar <piyushbagoria018@gmail.com>
2025-02-15 21:28:42 +0530

    Some Important utils

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => { 
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+     })
+})
+.catch((err) => console.error("Error connecting to MongoDB:", err));
 
 /*
 import express from "express";
